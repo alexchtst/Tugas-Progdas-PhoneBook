7a9917843063d074d12987ac489548c4b28daa22
alexchtst-01 <acinatra@gmail.com>
2025-05-12 20:57:24 +0700

    feat: create load and read csv data

--- LOAD_PANEL.cpp
+++ LOAD_PANEL.cpp
@@ -3,6 +3,9 @@
 */
 
 #include "LOAD_PANEL.h"
+#include <wx/textfile.h>
+#include <wx/filedlg.h>
+#include <wx/tokenzr.h>
 
 LOAD_PANEL::LOAD_PANEL(wxNotebook *parent)
     : wxPanel(parent, wxID_ANY)
@@ -43,4 +46,48 @@ void LOAD_PANEL::OnLoadCSV(wxCommandEvent &event)
         return;
 
     // LoadCSVFile(openFileDialog.GetPath());
+}
+
+void LOAD_PANEL::LoadCSVFile(const wxString &filepath)
+{
+    wxTextFile file;
+
+    // file not found exception
+    if (!file.Open(filepath))
+    {
+        wxMessageBox("Gagal membuka file!", "Error", wxICON_ERROR);
+        return;
+    }
+
+    // read file and buffer it
+    wxArrayString lines;
+    for (wxString str = file.GetFirstLine(); !file.Eof(); str = file.GetNextLine())
+        lines.Add(str);
+
+    file.Close();
+
+    // empty csv file handling
+    if (lines.IsEmpty())
+        return;
+    
+    
+    // write every fucking data in grid
+    grid->ClearGrid();
+    if (grid->GetNumberRows() > 0)
+        grid->DeleteRows(0, grid->GetNumberRows());
+    if (grid->GetNumberCols() > 0)
+        grid->DeleteCols(0, grid->GetNumberCols());
+
+    wxArrayString headers = wxSplit(lines[0], ',');
+    grid->AppendCols(headers.GetCount());
+    for (size_t c = 0; c < headers.GetCount(); ++c)
+        grid->SetColLabelValue(c, headers[c]);
+
+    for (size_t r = 1; r < lines.GetCount(); ++r)
+    {
+        wxArrayString rowData = wxSplit(lines[r], ',');
+        grid->AppendRows(1);
+        for (size_t c = 0; c < rowData.GetCount(); ++c)
+            grid->SetCellValue(r - 1, c, rowData[c]);
+    }
 }
